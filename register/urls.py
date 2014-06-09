1cfd600c7559acd0c0d40eaf9021eb6816a76540
mszewczyk <bierkionline@gmail.com>
2014-06-09 19:29:02 +0200

    podstawowe logowanie?

---
+++ register/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.zarejestruj, name='zarejestruj'),
+    url(r'zaloguj/$', views.zaloguj, name="zaloguj"),
+    url(r'logowanie/$', views.logowanie, name="logowanie"),
+    url(r'rejestracja/$', views.rejestracja, name="rejestracja"),
+	url(r'zarejestruj/$', views.zarejestruj, name="zarejestruj"),
+    url(r'wyloguj/$', views.wyloguj, name="wyloguj"),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'static'})
+)
--- sig/settings.py
+++ sig/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'article',
     'invoice',
+	'register',
 )
 
 MIDDLEWARE_CLASSES = (
